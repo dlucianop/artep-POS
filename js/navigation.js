6529d85a877e8a363754732a34e20eedd1ca64c3
dlucianop <3112dlp@gmail.com>
2024-12-06 23:21:42 -0600

    Se agrego el la fecha y hora, ademas de la navegacion entre las pantallas

--- js/navigation.js
+++ js/navigation.js
@@ -1,19 +1,24 @@
-document.getElementById("inventory-Btn").addEventListener('click', () => {
-    window.location.href = "html/inventory.html";
+document.addEventListener('DOMContentLoaded', () => {
+    
+    document.getElementById("inventory-Btn").addEventListener('click', () => {
+        window.location.href = "html/inventory.html"
+    });
 });
 
-document.getElementById("pos-Btn").addEventListener('click', () => {
-    window.location.href = "html/pos.html";
-});
 
-document.getElementById("sales-Btn").addEventListener('click', () => {
-    window.location.href = "html/sales.html";
-});
+/*document.addEventListener("DOMContentLoaded", () => {
+    const dashboardBtn = document.getElementById("dashboard-Btn");
+    if (dashboardBtn) {
+        dashboardBtn.addEventListener("click", () => {
+            alert("¡Botón presionado!");
+        });
+    } else {
+        console.error("No se encontró el botón con ID 'dashboard-Btn'.");
+    }
+});*/
 
-document.getElementById("configuration-Btn").addEventListener('click', () => {
-    window.location.href = "html/configuration.html";
-});
 
-document.getElementById("dashboard-Btn").addEventListener('click', () => {
-    window.location.href = "index.html";
-});
+/*<button id="inventory-Btn">Inventario</button>
+            <button id="pos-Btn">Punto de Venta</button>
+            <button id="sales-Btn">Historial de Ventas</button>
+            <button id="configuration-Btn">Configuración</button>*/
