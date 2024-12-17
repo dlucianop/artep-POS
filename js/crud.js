45110838679e0a1d2ae9ca104a4516b41f68f70e
dlucianop <3112dlp@gmail.com>
2024-12-17 12:26:06 -0600

    Se agrego la funcionalidad de Agregar un Producto

--- js/crud.js
+++ js/crud.js
@@ -2,17 +2,68 @@ const path = require('path')
 const dbJS = path.join(__dirname,'..', 'js', 'database.js');
 const { openDataBase, closeDatabase } = require(dbJS);
 
-function createProduct(){
-    //
+function createProduct(product, callback) {
+    const db = openDataBase();
+    
+    const query = `
+        INSERT INTO productos (code, category, model, size, decoration, color)
+        VALUES (?, ?, ?, ?, ?, ?);
+    `;
+
+    db.run(query, [product.code, product.category, product.model, product.size, product.decoration, product.color], function(err) {
+        if (err) {
+            console.error('ERROR al insertar el producto:', err.message);
+            closeDatabase(db);
+            callback(err, null);
+            return;
+        } 
+        console.log(`Producto creado con el ID: ${this.lastID}`);
+        
+        const db2 = openDataBase();
+        const query2 = `
+            INSERT INTO estado_producto (state, code, num_products)
+            VALUES ("CRUDO", ?, 0);
+        `;
+
+        db2.run(query2, [product.code], function(err) {
+            if (err) {
+                console.error('ERROR al insertar estado del producto:', err.message);
+                closeDatabase(db2);
+                callback(err, null);
+                return;
+            }
+            console.log(`Estado de producto creado con el ID: ${this.lastID}`);
+            
+            closeDatabase(db2);
+            callback(null, { id: this.lastID, ...product });
+        });
+    });
 }
+
+
 
 function readProducts(callback) {
     const db = openDataBase();
-    db.all('SELECT p.*, ep.state, ep.num_products ' +
-           'FROM productos p ' +
-           'INNER JOIN estado_producto ep ' +
-           'ON p.code = ep.code ' +
-           'WHERE ep.state = "TERMINADO";', [], (err, rows) => {
+
+    const query = `
+        SELECT p.*, 
+            ep.state, 
+            CASE 
+                WHEN ep.state = "TERMINADO" THEN ep.num_products
+                ELSE 0 
+            END AS num_products
+        FROM productos p 
+        INNER JOIN estado_producto ep
+        ON p.code = ep.code
+        ORDER BY 
+            CASE 
+                WHEN ep.state = "TERMINADO" AND ep.num_products > 0 THEN 0 
+                ELSE 1 
+            END,
+        p.code;
+    `;
+    
+    db.all(query, [], (err, rows) => {
         if (err) {
             console.error('ERROR al ejecutar la consulta:', err.message);
             callback(err, null);
--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const { join } = require('path');
 const crudJS = join(__dirname, '..', 'js', 'crud.js');
-const { readProducts } = require(crudJS);
+const { createProduct, readProducts, updateProduct, deleteProduct } = require(crudJS);
 
 function fillTable(products) {
     const tableBody = document.querySelector('#inventoryTable tbody');
@@ -13,11 +13,11 @@ function fillTable(products) {
         row.innerHTML = `
             <td>${product.code || 'N/A'}</td>
             <td>${product.category || 'N/A'}</td>
-            <td>${product.model || 'N/A'}</td>
+            <td class="full-width">${product.model || 'N/A'}</td>
             <td>${product.size || 'N/A'}</td>
-            <td>${product.decoration || 'N/A'}</td>
-            <td>${product.color || 'N/A'}</td>
-            <td>${product.num_products || 0}</td>
+            <td class="full-width">${product.decoration || 'N/A'}</td>
+            <td class="full-width">${product.color || 'N/A'}</td>
+            <td style="background-color: ${product.num_products === 0 ? '#B03A2E' : '#6B8E23'};" class="prodStock">${product.num_products || 0}</td>
             <td class="btnAction">
                 <button onclick="editProduct(${product.code})" id="btnUpdate">Modificar</button>
                 <button onclick="deleteProduct(${product.code})" id="btnDelete">Eliminar</button>
@@ -29,6 +29,44 @@ function fillTable(products) {
     tableBody.appendChild(fragment);
 }
 
+///************************** AGREGAR PRODUCTO */
+document.getElementById('formProduct').addEventListener('submit', (event) => {
+    event.preventDefault();
+    const modal = document.getElementById("modal");
+    const form = document.getElementById("formProduct");
+
+    const newProduct = {
+        code: document.getElementById('codProd').value,
+        category: document.getElementById('categoryProd').value,
+        model: document.getElementById('modelProd').value,
+        size: document.getElementById('sProd').value,
+        decoration: document.getElementById('decorationProd').value,
+        color: document.getElementById('colorProd').value,
+    };
+
+    //console.log(newProduct)
+    createProduct(newProduct, (err, createdProduct) => {
+        if (err) {
+            console.error('Error al crear el producto:', err);
+        } else {
+            readProducts((err, data) => {
+                if (err) {
+                    document.querySelector('#error-message').textContent = `Error al leer productos: ${err}`;
+                } else {
+                    alert("Se agrego el producto al Inventario con exito");
+                    modal.style.display = "none";
+                    form.reset();
+                    if (typeof isFormDirty !== 'undefined') {
+                        isFormDirty = false;
+                    }
+                    fillTable(data);
+                }
+            });
+            
+        }
+    });
+});
+
 readProducts((err, data) => {
     if (err) {
         document.querySelector('#error-message').textContent = `Error al leer productos: ${err}`;
@@ -36,9 +74,6 @@ readProducts((err, data) => {
         fillTable(data);
     }
 });
-
-///************************** AGREGAR PRODUCTO */
-
 
 //*************************** BUSCAR PRODUCTO */
 
