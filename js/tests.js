dad79817cd1cac21767779362e9fb2c61230ce2b
dlucianop <3112dlp@gmail.com>
2025-04-22 21:29:37 -0600

    Se cambio los callback del CRUD de produccion

--- js/tests.js
+++ js/tests.js
@@ -1,190 +1,82 @@
-function readVentasDetalle(detalles_venta, callback) {
+const path = require('path')
+const dbJS = path.join(__dirname,'..', 'js', 'connection.js');
+const { openDataBase, closeDatabase } = require(dbJS);
+const toast = join(__dirname, "..", "js", "toast.js");
+const { showToast, ICONOS } = require(toast);
+
+function createOrden(orden, callback) {
     const db = openDataBase();
     const query = `
-        SELECT 
-            v.id_venta AS noDeVenta, v.fecha_venta, v.hora AS hora_venta, 
-            v.nombre AS cliente_name, v.telefono, v.correo, v.domicilio, v.fecha_entrega, v.metodo_pago, v.forma_pago,
-            ip.code AS codigo, ip.category || ' MOD.' || ip.model || ' TAM.' || ip.size || ' DECOR.' || ip.decoration || ' COL.' || ip.color AS nombre, dv.price AS precioUnit, dv.quantity AS cantidad, dv.importe,
-            v.monto, v.pago, (v.pago - v.monto) AS cambio
-        FROM ventas v
-        INNER JOIN detalles_venta dv
-        ON v.id_venta = dv.id_venta
-        INNER JOIN inventario_productos ip
-        ON ip.code = dv.code
-        WHERE v.id_venta = ?;
+        INSERT INTO orden_produccion
+            (id_venta, id_origen, id_fase, fecha_entrega, categoria, size, cantidad_inicial, cantidad_buenos, cantidad_rotos, cantidad_deformes) 
+        VALUES (?, ?, 1, ?, ?, ?, ?, 0, 0, 0);
     `;
 
-    try {
-        db.all(query, [
-            detalles_venta.id_ventaVD
-        ], (err, rows) => {
+    db.run(query, 
+        [
+            orden.id_ventaO,
+            orden.id_origenO,
+            orden.fecha_entregaO,
+            orden.categoriaO, 
+            orden.sizeO, 
+            orden.cantidad_inicialO
+        ], 
+        function (err) {
             if (err) {
-                console.error(`[ERROR] Consulta fallida: ${err.message}`);
+                closeDatabase(db);
                 callback(err, null);
                 return;
             }
-            console.log(`[INFO] Consulta ejecutada con éxito. Filas obtenidas: ${rows.length}`);
-            callback(null, rows);
-        });
-    } catch (err) {
-        console.error(`[CRITICAL] Error inesperado: ${err.message}`);
-        callback(err, null);
-    } finally {
-        closeDatabase(db);
-        console.log('[INFO] Conexión a la base de datos cerrada.');
-    }
-}
 
-
-
-function deleteVentaDetalle(detalles_venta, callback){
-    const db = openDataBase();
-    const query = `
-        DELETE
-        FROM detalles_venta
-        WHERE id_venta = ?;
-    `;
-
-    db.run(
-        query,
-        [
-            detalles_venta.id_ventaV
-        ],
-        function (err) {
-            if (err) {
-                console.error(`[ERROR] Consulta fallida: ${err.message}`);
+            setTimeout(() => {
+                const newOrden = {
+                    id_venta: orden.id_ventaO,
+                    id_origen: orden.id_origenO,
+                    fecha_entreg: orden.fecha_entregaO,
+                    categoria: orden.categoriaO, 
+                    size: orden.sizeO, 
+                    cantidad_inicial: orden.cantidad_inicialO,
+                    cantidad_buenos: 0,
+                    cantidad_rotos: 0,
+                    cantidad_deformes: 0,
+                };
                 closeDatabase(db);
-                return callback(err, null);
-            }
-
-            console.log(`Venta con codigo ${datos_venta.id_ventaV} eliminada correctamente.`);
-
-            const deleteddetalles = {
-                id_venta: detalles_venta.id_ventaV, 
-            };
-
-            closeDatabase(db);
-            callback(null, deleteddetalles);
+                callback(null, newOrden);
+            }, 500);
         }
     );
 }
 
-
-function deleteVenta(id_venta){
-    return new Promise((resolve, reject) => {
-        const db = openDataBase();
-        const query = `
-            DELETE FROM ventas
-            WHERE id_venta = ?;
-        `;
-
-        db.run(query, [id_venta], function (err) {
-            try {
-                if (err) {
-                    return reject(new Error("Error al borrar venta: " + err.message));
-                }
-    
-                if (this.changes === 0) {
-                    return reject(new Error("No se borró ninguna venta (¿existe ese identificador?)"));
-                }
-                
-                resolve(`Venta con identificador'${id_venta}' eliminado correctamente`);
-            } catch (err) {
-                reject(err);
-            } finally {
-                closeDatabase(db);
+function readOrdenbyFASE(orden, callback) {
+    const db = openDataBase();
+    const query = `
+        SELECT 
+            op.id_orden, op.id_venta, oo.nombre AS origen, f.name_fase AS fase_actual, op.fecha_entrega,
+            op.categoria, op.size, op.cantidad_inicial, op.cantidad_buenos, op.cantidad_rotos, op.cantidad_deformes
+        FROM orden_produccion op
+        INNER JOIN origen_orden oo
+        ON op.id_origen = oo.id_origen
+        INNER JOIN fase f
+        ON f.id_fase = op.id_fase
+        WHERE op.id_fase = ?
+    `;
+    try {
+        db.all(query, 
+            [
+                orden.faseO
+            ], 
+        (err, rows) => {
+            if (err) {
+                callback(err, null);
+                return;
             }
+            callback(null, rows);
         });
-    });
+    } catch (err) {
+        callback(err, null);
+    } finally {
+        closeDatabase(db);
+    }
 }
 
-
-
-function readDetalles(id_venta) {
-    return new Promise((resolve, reject) => {
-        const db = openDataBase();
-        const query = `
-            SELECT 
-                v.id_venta AS noDeVenta, v.fecha_venta, v.hora AS hora_venta, 
-                v.nombre AS cliente_name, v.telefono, v.correo, v.domicilio, v.fecha_entrega, v.metodo_pago, v.forma_pago,
-                ip.code AS codigo, 
-                ip.category || ' MOD.' || ip.model || ' TAM.' || ip.size || ' DECOR.' || ip.decoration || ' COL.' || ip.color AS nombre, 
-                dv.price AS precioUnit, dv.quantity AS cantidad, dv.importe,
-                v.monto, v.pago, (v.pago - v.monto) AS cambio
-            FROM ventas v
-            INNER JOIN detalles_venta dv ON v.id_venta = dv.id_venta
-            INNER JOIN inventario_productos ip ON ip.code = dv.code
-            WHERE v.id_venta = ?;
-        `;
-
-        db.all(query, [id_venta], (err, rows) => {
-            try {
-                if (err) {
-                    return reject(new Error("Error al obtener los detalles de la venta: " + err.message));
-                }
-
-                if (!rows || rows.length === 0) {
-                    return resolve([]);
-                }
-
-                resolve(rows);
-            } catch (err) {
-                reject(err);
-            } finally {
-                closeDatabase(db);
-            }
-        });
-    });
-}
-
-
-
-function searchVenta(idVenta) {
-    return new Promise((resolve, reject) => {
-        const db = openDataBase();
-        const query = `
-            SELECT 
-                v.id_venta as No_venta,
-                v.fecha_venta,
-                v.hora,
-                v.nombre as nombreCliente,
-                v.telefono,
-                v.correo,
-                v.domicilio,
-                v.fecha_entrega,
-                v.metodo_pago,
-                v.forma_pago,
-                ip.code as codigoProd,
-                dv.price as precioUnit,
-                dv.quantity as cantidad,
-                dv.importe as importe,
-                ip.category || " "|| ip.model || " [Tamaño " || ip.size || "]" || " Decoracion " || ip.decoration || " - Color " || ip.color as descripcion,
-                v.monto,
-                v.pago
-            FROM detalles_venta dv
-            INNER JOIN inventario_productos ip ON dv.code = ip.code
-            INNER JOIN ventas v ON v.id_venta = dv.id_venta
-            WHERE dv.id_venta = ?;
-        `;
-
-        db.all(query, [idVenta], (err, rows) => {
-            try {
-                if (err) {
-                    return reject(new Error("Error al consultar la venta: " + err.message));
-                }
-
-                if (!rows || rows.length === 0) {
-                    return resolve([]); // o `null` si prefieres
-                }
-
-                resolve(rows);
-            } catch (errFinal) {
-                reject(errFinal);
-            } finally {
-                closeDatabase(db);
-                console.log('[INFO] Conexión a la base de datos cerrada.');
-            }
-        });
-    });
-}
-
+module.exports = { createOrden, readOrdenbyFASE }
