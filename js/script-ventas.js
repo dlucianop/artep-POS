42d31912900d21d62f468a7bb1f8842e014d2fc7
dlucianop <3112dlp@gmail.com>
2025-06-04 09:05:10 -0600

    Se agrego la funcionalidad de marcar venta como entregada, y se actualiza el stock_apartado del producto

--- js/script-ventas.js
+++ js/script-ventas.js
@@ -8,7 +8,19 @@ const {
     createDetalle,
     readDetalles,
     deleteVentaConDetalles,
+    updateEstadoVenta
 } = require(join(__dirname, '..', 'js', 'crud-ventas.js'));
+const {
+    readOrdenes, createOrden, updateEstado, readOrdenesOrigen
+} = require(join(__dirname, "..", "js", "crud-produccion.js"));
+const { 
+    createProducto, 
+    readProductos, 
+    searchProduct, 
+    updateProducto, 
+    deleteProducto,
+    updateStockProducto
+} = require(join(__dirname, '..', 'js', 'crud-productos.js'));
 const { 
     showToast, 
     showConfirmToast, 
@@ -25,6 +37,10 @@ async function initVentas() {
         const ventas = await readVentas();
         window.ventas = ventas;
         fillTableVentas(ventas);
+
+        const productos = await readProductos();
+        window.productos = productos;
+
         console.log('📦 Se cargaron ventas.');
     } catch (error) {
         console.error('❌ Error al cargar ventas:', error.message);
@@ -64,25 +80,71 @@ function fillTableVentas(sales) {
     sales.forEach(sale => {
         const fechaEntrega = formatDate(sale.fecha_entrega);
         const row = document.createElement('tr');
+        const isEntregada = sale.entregada === 1;
+        const disabledAttr = isEntregada ? 'disabled' : '';
+
         row.innerHTML = `
         <td>${sale.id_venta || 'N/A'}</td>
         <td>${sale.fecha_venta || 'N/A'}</td>
         <td>${sale.monto || 'N/A'}</td>
         <td>${fechaEntrega || 'N/A'}</td>
         <td>
-            <select onchange="estadoVenta(${sale.id_venta}, this.value)">
-                <option value="0" ${!sale.entregada ? 'selected' : ''}>Por entregar</option>
-                <option value="1" ${sale.entregada ? 'selected' : ''}>Entregada</option>
+            <select data-id="${sale.id_venta}" onchange="estadoVenta(${sale.id_venta}, this.value)">
+                <option value="1" ${sale.entregada === 1 ? 'selected' : ''}>Entregada</option>
+                <option value="0" ${sale.entregada === 0 ? 'selected' : ''}>No entregada</option>
             </select>
         </td>
         <td class="col-btn">
             <button type="button" onclick="imprimirVenta(${sale.id_venta})">🖨️ Imprimir</button>
         </td>
         `;
+
         fragment.appendChild(row);
     });
 
     tableBody.appendChild(fragment);
+}
+
+async function estadoVenta(id, estadoSelect) {
+    try {
+        const estado = parseInt(estadoSelect);
+
+        if (estado === 1) {
+            const productos = await readDetalles(id);
+            for (const item of productos) {
+                const existeProducto = window.productos.find(p => p.code === item.codigo);
+                if (!existeProducto) continue;
+
+                const payload = {
+                    code: existeProducto.code,
+                    stock_apartado: existeProducto.stock_apartado - item.cantidad,
+                    stock_disponible: existeProducto.stock_disponible,
+                    stock_en_proceso: existeProducto.stock_en_proceso
+                };
+
+                //console.log("📦 Producto a actualizar:", payload);
+                await updateStockProducto(payload);
+            }
+
+            const confirmed = await showConfirmDialog(
+                `¿Desea terminar de marcar esta venta como ENTREGADA? Esta acción es IRREVERSIBLE.`,
+                "Marcar venta como ENTREGADA"
+            );
+
+            if (confirmed) {
+                await updateEstadoVenta(id);
+                showToast("Estado de entrega actualizado correctamente.", ICONOS.exito);
+            } else {
+                const select = document.querySelector(`#table-ventas select[data-id='${id}']`);
+                if (select) {
+                    select.value = "0";
+                }
+            }
+        }
+    } catch (error) {
+        console.error("❌ Error al actualizar estado de entrega:", error);
+        showToast("No se pudo actualizar el estado.", ICONOS.error);
+    }
 }
 
 async function imprimirVenta(id) {
@@ -106,20 +168,28 @@ async function imprimirVenta(id) {
     }
 }
 
-/*function editarVenta(id) {
-    console.log('Editando venta', id);
-}
 
-function eliminarVenta(id) {
-    if (!confirm('¿Seguro que quieres eliminar la venta #' + id + '?')) return;
-}*/
+function showConfirmDialog(message = "¿Estás seguro?", title = "Confirmar acción") {
+    return new Promise((resolve) => {
+        const dialog = document.getElementById('confirm-dialog');
+        const titleEl = document.getElementById('confirm-title');
+        const messageEl = document.getElementById('confirm-message');
+        const yesBtn = document.getElementById('confirm-yes');
+        const noBtn = document.getElementById('confirm-no');
 
-async function estadoVenta(id, estado) {
-    try {
-        /*actualizar inventario Productos, eliminar de stock apartados */
-        showToast("Estado de entrega actualizado correctamente.", ICONOS.exito);
-    } catch (error) {
-        console.error("Error al actualizar estado de entrega:", error);
-        showToast("No se pudo actualizar el estado.", ICONOS.error);
-    }
+        titleEl.textContent = title;
+        messageEl.textContent = message;
+
+        yesBtn.onclick = () => {
+            dialog.close();
+            resolve(true);
+        };
+
+        noBtn.onclick = () => {
+            dialog.close();
+            resolve(false);
+        };
+
+        dialog.showModal();
+    });
 }
